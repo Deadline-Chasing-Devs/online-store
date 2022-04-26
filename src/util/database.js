489725d8694a0036e0305b26f567deb121b65ad1
Supun Dasanayaka <supun.19@cse.mrt.ac.lk>
2022-04-26 09:42:37 +0530

    add new database methods. add order class

--- src/util/database.js
+++ src/util/database.js
@@ -55,9 +55,66 @@ const getItemById = async (pool, itemId) => {
 };
 
 // Get order by orderId
+const getOrderById = async (pool,orderId) => {
+    let results;
+    try{
+        results = await queryPromise(
+            pool,
+            "SELECT * FROM order WHERE order_id=?",
+            [orderId]
+        );
+    }catch (error){
+        throw "Database Error";
+    }
+
+    if(results.length){
+        return new Order(
+            results[0].order_id,
+            results[0].customer_name,
+            results[0].customer_email,
+            results[0].customer_address,
+            results[0].customer_contact_num,
+            results[0].status,
+            results[0].date_time
+        );
+    }
+};
 
 // Get items in an order by orderId
+const getItemsByOrderId = async (pool,orderId) => {
+    let results;
+    try {
+        results = await queryPromise(
+            pool,
+            "SELECT item_id FROM order_item WHERE order_id=?",
+            [orderId]
+        );
+    }catch (error){
+        throw "Database Error";
+    }
+
+    let i = 0;
+    const items = [];
+    while (i < results.length){
+        items[i] = getItemById(results[i]);
+    }
+    return items;
+}
 
 // Get image_ids of an item by itemId
+const getImageIdsByItemId = async (pool,itemId) => {
+    let results;
+    try {
+        results = await queryPromise(
+            pool,
+            "SELECT image_id FROM item_image WHERE item_id = ?",
+            [itemId]
+        );
+    }catch (error){
+        throw "Database Error";
+    }
+
+    return results;
+}
 
 export { getUserByUsername, getItemById };
