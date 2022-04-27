0fb4ecbe57478020cca4549c188c7e273db0aa14
RansikaC99 <rumal.19@cse.mrt.ac.lk>
2022-04-27 22:43:13 +0530

    update homepage

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { authChecker } from "../util/middleware.js";
 
 const handler = () => {
     const homepageRouter = express.Router();
 
     // handle route
-    homepageRouter.get("", authChecker, (req, res) => {
+    homepageRouter.get("", (req, res) => {
         res.render("homepage");
     });
 
