acad9b859c6adb724e91b0c6ee2f141ebf066cec
Nimantha Cooray <nimanthacooray@gmail.com>
2022-04-27 18:10:11 +0530

    Modify handler of get `edit-item` route
    - Send item details of the given id

--- src/routes/editItem.js
+++ src/routes/editItem.js
@@ -1,11 +1,16 @@
 import express from "express";
+import { getItemById } from "../util/database.js";
 import { authChecker } from "../util/middleware.js";
 
-const handler = () => {
+const handler = (pool) => {
     const editItemRouter = express.Router();
 
-    editItemRouter.get("", authChecker, (req, res) => {
-        res.render("editItem");
+    editItemRouter.get("/:id", authChecker, async (req, res) => {
+        const itemId = req.params.id;
+        const item = await getItemById(pool, itemId);
+
+        if (!item) res.redirect("/dashboard");
+        res.render("editItem", { item: { ...item } });
     });
 
     return editItemRouter;
