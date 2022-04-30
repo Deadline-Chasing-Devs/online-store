c70b8938750d4c937bf0e266a0c54c4bf54b113d
poornaSavindi <prnsavindi11@gmail.com>
2022-04-30 13:05:39 +0530

    add route to the login page

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { sessionChecker } from "../util/middleware.js";
 
 const handler = () => {
     const orderRouter = express.Router();
 
-    // handle route
-
+    orderRouter.get("", sessionChecker, (req, res) => {
+        res.render("order");
+    });
     return orderRouter;
 };
 
