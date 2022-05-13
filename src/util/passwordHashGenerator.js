d038aca57dec8c7b9ae89b8f7e3be41d316ce6c3
Nimantha Cooray <nimanthacooray@gmail.com>
2022-05-13 22:52:59 +0530

    Modify passwordHashGenerator
    Use cmd args to produce the hash

--- src/util/passwordHashGenerator.js
+++ src/util/passwordHashGenerator.js
@@ -1,7 +1,8 @@
 import bcrypt from "bcrypt";
+import process from "node:process";
 
 const generateHash = (password) => {
     return bcrypt.hashSync(password, 10);
 };
-
-console.log(generateHash("yourpassword"));
+console.log(process.argv[2]);
+console.log(generateHash(process.argv[2]));
