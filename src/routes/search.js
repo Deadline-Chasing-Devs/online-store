e30fde6f5d2f949c5a21343889ace9ee093c3333
Nimantha Cooray <nimanthacooray@gmail.com>
2022-05-14 21:27:46 +0530

    Implement basic search route

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { searchItemByName } from "../util/database.js";
 
-const handler = () => {
+const handler = (pool) => {
     const searchRouter = express.Router();
 
     // handle route
+    searchRouter.get("", async (req, res) => {
+        const name = req.query.name;
+        const results = await searchItemByName(pool, name);
+        // console.log(results);
+        res.json(results);
+    });
 
     return searchRouter;
 };
