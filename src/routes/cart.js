5f5881fe6fb93a7af63f044b5ed5d8f3a0aac9ad
RansikaC99 <89589832+RansikaC99@users.noreply.github.com>
2022-05-20 19:09:37 +0530

    Create cart nav

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,6 +4,9 @@ const handler = () => {
     const cartRouter = express.Router();
 
     // handle route
+    cartRouter.get("", (req, res) => {
+        res.render("cart");
+    });
 
     return cartRouter;
 };
