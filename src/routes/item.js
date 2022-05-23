953446ffee254f736bb06296b94590e1761cc468
RansikaC99 <89589832+RansikaC99@users.noreply.github.com>
2022-05-23 15:49:04 +0530

    create item page

--- src/routes/item.js
+++ src/routes/item.js
@@ -1,15 +1,12 @@
 import express from "express";
-import {getItemById} from "../util/database.js";
 
 const handler = (pool) => {
     const itemRouter = express.Router();
 
     // handle route
 
-    itemRouter.get("/:item_id", async (req,res) => {
-        const itemId = req.params.item_id;
-        const item = await getItemById(pool,itemId);
-        res.render("item",{item: item});
+    itemRouter.get("", (req, res) => {
+        res.render("item");
     });
 
     return itemRouter;
