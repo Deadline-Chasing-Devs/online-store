3ebe36384ddfb5ec47e488c7dc3fd03974f6d07c
Nimantha Cooray <nimanthacooray@gmail.com>
2022-05-23 12:10:28 +0530

    Connect '/new-item' page to backend
    - Added code to preview the images selected to upload.

--- public/javascript/vendorNewItem.js
+++ public/javascript/vendorNewItem.js
@@ -1,11 +1,11 @@
 window.onload = function () {
     //Check File API support
     if (window.File && window.FileList && window.FileReader) {
-        var filesInput = document.getElementById("files");
+        var filesInput = document.getElementById("images");
 
         filesInput.addEventListener("change", function (event) {
             var files = event.target.files; //FileList object
-            var output = document.getElementById("result");
+            var output = document.getElementById("images-view");
 
             for (var i = 0; i < files.length; i++) {
                 var file = files[i];
@@ -23,12 +23,12 @@ window.onload = function () {
                     div.className = "col-sm-3";
 
                     div.innerHTML =
-            "<img class='thumbnail col-sm-3' style='height : 200px; width : auto; background-size: cover; background-repeat: no-repeat; background-position:center;' src='" +
-            picFile.result +
-            "'" +
-            "title='" +
-            picFile.name +
-            "'/>";
+                        "<img class='thumbnail col-sm-3' style='height : 200px; width : auto; background-size: cover; background-repeat: no-repeat; background-position:center;' src='" +
+                        picFile.result +
+                        "'" +
+                        "title='" +
+                        picFile.name +
+                        "'/>";
 
                     output.insertBefore(div, null);
                 });
@@ -41,3 +41,17 @@ window.onload = function () {
         console.log("Your browser does not support File API");
     }
 };
+
+function showCoverPhoto(event) {
+    if (event.target.files.length == 1 && event.target.files[0].type.match("image")) {
+        let src = URL.createObjectURL(event.target.files[0]);
+        let preview = document.getElementById("cover-photo-view");
+        preview.src = src;
+        preview.style.display = "block";
+    }
+}
+
+function clearForm() {
+    document.getElementById("new-item-form").reset();
+    document.getElementById("cover-photo-view").setAttribute("src", "");
+}
