1a815a258a6ce663a02e2b1e4cf9c77e48cccf3b
Nimantha Cooray <nimanthacooray@gmail.com>
2022-05-25 07:47:43 +0530

    Fetch & show items list on '/dashboard' page

---
+++ public/javascript/dashboard.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+    fetch("/items", {
+        method: "GET",
+    })
+        .then((res) => {
+            if (res.status >= 200 && res.status < 300) {
+                return Promise.resolve(res);
+            } else {
+                return Promise.reject(new Error(res.statusText));
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            const table = document.getElementById("item-table");
+            data.rows.forEach((row) => {
+                let tableRow = table.insertRow();
+
+                let newCell1 = tableRow.insertCell();
+                let newText1 = document.createTextNode(row.name);
+                newCell1.appendChild(newText1);
+
+
+                let newCell2 = tableRow.insertCell();
+                let newText2 = document.createTextNode(row.price);
+                newCell2.appendChild(newText2);
+            });
+        });
+};
