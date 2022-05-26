6d8950b9cafbfb260c11ac8ca0772c0c19c8ad07
Nimantha Cooray <nimanthacooray@gmail.com>
2022-05-26 10:43:51 +0530

    Show order details on '/order/:id' page

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,17 +1,20 @@
 import express from "express";
-import {getOrderById} from "../util/database.js";
+import { getItemsByOrderId, getOrderById } from "../util/database.js";
 
 const handler = (pool) => {
     const orderRouter = express.Router();
 
     // handle route
 
-    orderRouter.get("/:order_id", async (req,res) => {
+    orderRouter.get("/:order_id", async (req, res) => {
         const orderId = req.params.order_id;
-        const order = await getOrderById(pool,orderId);
-        //const order = req.params.order_id;
-        res.render("order",{order: order});
-
+        const order = await getOrderById(pool, orderId);
+        if (!order) {
+            res.redirect("/dashboard");
+            return;
+        }
+        const orderItems = await getItemsByOrderId(pool, orderId);
+        res.render("order", { order, orderItems });
     });
     return orderRouter;
 };
