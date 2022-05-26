7cd6e954e0ad5f56ff1e7973016ee8d911f30f82
Nimantha Cooray <nimanthacooray@gmail.com>
2022-05-26 11:37:21 +0530

    Implement order status updating functionality

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { getItemsByOrderId, getOrderById } from "../util/database.js";
+import { getItemsByOrderId, getOrderById, updateOrderStatus } from "../util/database.js";
+import { authChecker } from "../util/middleware.js";
 
 const handler = (pool) => {
     const orderRouter = express.Router();
 
     // handle route
 
-    orderRouter.get("/:order_id", async (req, res) => {
+    orderRouter.get("/:order_id", authChecker, async (req, res) => {
         const orderId = req.params.order_id;
         const order = await getOrderById(pool, orderId);
         if (!order) {
@@ -14,8 +15,29 @@ const handler = (pool) => {
             return;
         }
         const orderItems = await getItemsByOrderId(pool, orderId);
-        res.render("order", { order, orderItems });
+        res.render("order", { order, orderItems, success: req.flash("success") });
     });
+
+    orderRouter.post("/:order_id", authChecker, async (req, res) => {
+        const orderId = req.params.order_id;
+        const order = await getOrderById(pool, orderId);
+        if (!order) {
+            res.redirect("/dashboard");
+            return;
+        }
+
+        const orderStatus = req.body.status;
+        const validStatuses = ["PAID", "PROCESSING", "DISPATCHED", "DELIVERED"];
+        if (!orderStatus || !validStatuses.includes(orderStatus)) {
+            res.redirect(`/order/${orderId}`);
+            return;
+        }
+
+        await updateOrderStatus(pool, orderId, orderStatus);
+        req.flash("success", "Order status updated successfully");
+        res.redirect(`/order/${orderId}`);
+    });
+
     return orderRouter;
 };
 
