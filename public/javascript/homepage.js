a1a66b67b3b4dd1ffbc947783bafc8c62fba1694
Supun Dasanayaka <supun.19@cse.mrt.ac.lk>
2022-05-28 16:34:17 +0530

    homepage and item/:id image addresses fixed. add search functionality to homepage

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -22,14 +22,11 @@ closeBtns.forEach((closeBtn)=>{
 function searchElement() {
     var input,txtvalue,products,div,i,h2;
     input = document.getElementById("pinput");
-    console.log(input.value);
     fileter = input.value.toLowerCase();
     product = document.getElementsByTagName('li');
-    console.log(product);
 
-    for(i = 6; i < product.length; i++){
+    for(i = 3; i < product.length; i++){
         h2 = product[i].getElementsByTagName("div")[0].getElementsByTagName("h2")[0];
-        console.log(h2.innerText);
         txtvalue = h2.innerText;
         if(txtvalue.toLowerCase().indexOf(fileter) > -1){
             product[i].style.display = "";
