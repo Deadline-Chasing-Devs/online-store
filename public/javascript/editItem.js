762229e8aa5a9cdea84d3bee6d46cd68671b7db7
Achira Dias <achira828@gmail.com>
2022-05-30 16:00:46 +0530

    Update editItem.js

--- public/javascript/editItem.js
+++ public/javascript/editItem.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+
     const removeItemBtn = document.getElementById("remove-btn");
     removeItemBtn.addEventListener("click", () => {
         const confirm = window.confirm(
@@ -16,6 +17,48 @@ window.onload = function () {
             });
         }
     });
+
+    //Check File API support
+    if (window.File && window.FileList && window.FileReader) {
+        var filesInput = document.getElementById("images");
+
+        filesInput.addEventListener("change", function (event) {
+            var files = event.target.files; //FileList object
+            var output = document.getElementById("images-view");
+
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+
+                //Only pics
+                if (!file.type.match("image")) continue;
+
+                var picReader = new FileReader();
+
+                picReader.addEventListener("load", function (event) {
+                    var picFile = event.target;
+
+                    var div = document.createElement("div");
+
+                    div.className = "col-sm-3";
+
+                    div.innerHTML =
+                        "<img class='thumbnail col-sm-3' style='height : 200px; width : auto; background-size: cover; background-repeat: no-repeat; background-position:center;' src='" +
+                        picFile.result +
+                        "'" +
+                        "title='" +
+                        picFile.name +
+                        "'/>";
+
+                    output.insertBefore(div, null);
+                });
+
+                //Read the image
+                picReader.readAsDataURL(file);
+            }
+        });
+    } else {
+        console.log("Your browser does not support File API");
+    }
 };
 
 function showCoverPhoto(event) {
@@ -25,4 +68,15 @@ function showCoverPhoto(event) {
         preview.src = src;
         preview.style.display = "block";
     }
+}
+
+var toDelList = [];
+function addToDelete(image){
+    document.getElementById(image).outerHTML = "";
+    toDelList.push(image);
+    console.log(toDelList);
+    document.getElementById("delArray").value = toDelList;
+}
+function discard(){
+    window.location.href = "/dashboard";
 }
