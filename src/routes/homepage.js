fd497915abc3cba1db0ab6461bde9551750534f0
Nimantha Cooray <nimanthacooray@gmail.com>
2022-06-06 07:05:09 +0530

    Fix bug in homepage route

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -10,11 +10,13 @@ const handler = (pool) => {
         const items = await getItemsForCustomer(pool,0,noofItems);
         let i;
         let availableItems = [];
-        var j = 0;
-        for(i=0; i < items.length; i++){
-            if(items[i].availability === 1){
-                availableItems[j] = items[i];
-                j++;
+        if (items) {
+            var j = 0;
+            for(i=0; i < items.length; i++){
+                if(items[i].availability === 1){
+                    availableItems[j] = items[i];
+                    j++;
+                }
             }
         }
         res.render("homepage",{items: availableItems, size: j});
